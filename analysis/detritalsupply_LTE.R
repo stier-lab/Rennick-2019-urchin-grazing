46564888c1603bb3740f491cf6d4ead59c9de959
Bart DiFiore <BartMBP@MacBook-Pro-6.local>
2021-07-26 11:23:23 -0400

    computer issues. immediate backup

--- analysis/detritalsupply_LTE.R
+++ analysis/detritalsupply_LTE.R
@@ -1,4 +1,16 @@
+library(here)
+library(tidyverse)
 
+npp <- read.csv("data/survey_data/NPP_ALL_YEARS_seasonal_with_MC_stderr.csv", stringsAsFactors = F,na.strings ="-99999") %>%
+  rename_all(tolower) %>%
+  dplyr::select(year, season, site, plnt_dns, frnd_dns, dry_wt,f, p, d, c, b, l) %>% 
+  filter(season == "3-Summer") %>%
+  mutate(det.r = f + b)
+
+av1 <- npp %>% summarize(ave = mean(det.r, na.rm = T), 
+                         sd = sd(det.r, na.rm = T)) # so approximately 2% of total biomass is lost as fronds and blades per day!
+av <- av1[1,1]
+sd <- av1[1,2]
 
 dt <- read.csv("data/survey_data/LTE_Detritus_Biomass_All_Years.csv", stringsAsFactors = F,na.strings ="-99999") %>%
   rename_all(tolower) %>% 
@@ -25,12 +37,12 @@ ggplot(dt, aes(x = detritus.gm2, y = predicted.detritus))+
   geom_point()+
   labs(x = expression(paste("Observed kelp detritus (g ",m^-2,")")), 
        y = expression(paste("Predicted detrital supply rate (g ",m^-2,d^-1,")")))+
-  theme_classic()
+  theme_classic()+
+  theme(text = element_text(size = 22))
 
-ggplot(dt, aes(x = wm_gm2, y = detritus))+
-  geom_point()
+ggsave(filename = here::here("figures", "appendixS2-figS1.png"), device = "png")
 
-lm1 <- lm(predicted.detritus ~ 0 + detritus, dt)
+lm1 <- lm(predicted.detritus ~ 0 + detritus.gm2, dt)
 summary(lm1)
 
 lm2 <- lm(detritus ~ wm_gm2, dt)
